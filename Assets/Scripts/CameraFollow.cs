5371b14ae3c5ddb92a076e5c78f05ded215137c0
spearsk <kspears@email.sc.edu>
2015-10-26 14:55:39 -0400

    Added player movement, character moves with wasd, turns with mouse hover. Camera follows player but does not follow the characters vision. NPC is now visible and is in the far back right corner of map.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,16 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CameraFollow : MonoBehaviour {
-    public GameObject player;
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;            // The position that that camera will be following.
+    public float smoothing = 5f;        // The speed with which the camera will be following.
 
-    // Use this for initialization
-    void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    Vector3 offset;                     // The initial offset from the target.
+
+    void Start()
+    {
+        // Calculate the initial offset.
+        offset = transform.position - target.position;
+    }
+
+    void FixedUpdate()
+    {
+        // Create a postion the camera is aiming for based on the offset from the target.
+        Vector3 targetCamPos = target.position + offset;
+
+        // Smoothly interpolate between the camera's current position and it's target position.
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
     }
 }
