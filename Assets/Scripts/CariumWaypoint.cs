870628ed2ab42d6e377552e6906425b28475d244
spearsk <kspears@email.sc.edu>
2015-10-29 03:09:03 -0400

    Readding files for comfort

---
+++ Assets/Scripts/CariumWaypoint.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CariumWaypoint : MonoBehaviour {
+    public Player player;
+    bool isIn = false;
+
+
+    void OnTriggerEnter(Collider col)
+    {
+            Debug.Log("Player entered the trigger");
+            isIn = player.isNowNear();
+
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+            Debug.Log("Player left the trigger");
+            isIn = player.isNotNear();
+    }
+
+    public bool isTrue()
+    {
+        return isIn;
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/SpawnParts.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class SpawnParts : MonoBehaviour {
+    // mutiple prefab objects
+    public Transform[] prefab;
+    public bool SpawnStuff = false;
+    Player pl;
+
+    public void SpawnAllParts()
+    {
+         Object.Instantiate(prefab[0], new Vector3(-8f, 2, -12f), transform.rotation);
+         Object.Instantiate(prefab[1], new Vector3(-57f, 2, 21f), transform.rotation);
+         Object.Instantiate(prefab[2], new Vector3(-104f, 3, 35f), transform.rotation);
+        Object.Instantiate(prefab[3], new Vector3(-33f, 2, 78f), transform.rotation);
+
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        if(SpawnStuff == true)
+        {
+            SpawnAllParts();
+        }
+    }
+}
